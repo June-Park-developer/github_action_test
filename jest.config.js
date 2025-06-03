9a9252e3b38a4945543b1c6f645d7583644f0e9d
JungEun Park <jungeunp97@naver.com>
2025-06-03 18:35:27 +0900

    [feat] : sum  추가

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const { createDefaultPreset } = require("ts-test");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
